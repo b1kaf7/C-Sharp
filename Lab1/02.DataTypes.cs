a375574ed3e7bfc612a45adc765824d1255cbf14
b1kaf7 <kaflebinamra@gmail.com>
2025-08-17 16:03:15 +0545

    Add variables demo program

---
+++ Lab1/02.DataTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int myInt = 25;
+        double myDouble = 3.1416;
+        char myChar = 'A';
+        bool myBool = true;
+        string myString = "C# Programming";
+
+        Console.WriteLine($"Integer: {myInt}, Double: {myDouble}, Char: {myChar}, Bool: {myBool}, String: {myString}");
+    }
+}
