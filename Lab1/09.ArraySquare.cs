8e92af73ce998c904cb3f8a11b04bd8923623f78
b1kaf7 <kaflebinamra@gmail.com>
2025-08-23 18:15:53 +0545

    Add program that prints square of the array elements

---
+++ Lab1/09.ArraySquare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numsArray = { 2, 4, 6, 8 };
+
+        foreach (int num in numsArray)
+        {
+            Console.WriteLine($"{num} squared is {num * num}");
+        }
+    }
+}
