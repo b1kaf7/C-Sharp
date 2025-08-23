0486fbbe38f63172fa654d4b485af620f91a44f6
b1kaf7 <kaflebinamra@gmail.com>
2025-08-23 18:15:08 +0545

    Add store students name program

---
+++ Lab1/08.StoreSutdentName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> students = new List<string> { "Rohan", "Binamra", "Ajit" };
+
+        foreach (string student in students)
+        {
+            Console.WriteLine($"Student: {student}");
+        }
+    }
+}
