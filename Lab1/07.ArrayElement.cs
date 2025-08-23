0714982ce9998401e053ce448e8e53f23358936d
b1kaf7 <kaflebinamra@gmail.com>
2025-08-23 18:14:02 +0545

    Print array elements

---
+++ Lab1/07.ArrayElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine($"Element {i + 1}: {numbers[i]}");
+        }
+    }
+}
