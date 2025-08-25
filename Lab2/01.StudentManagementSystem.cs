1410389dab7a8a117967e5ff4db1f4660285094a
b1kaf7 <kaflebinamra@gmail.com>
2025-08-25 20:04:33 +0545

    Deleted 2 files

---
+++ Lab2/01.StudentManagementSystem.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace StudentManagementSystem
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public Student()
+        {
+            StudentID = 0;
+            FirstName = "";
+            LastName = "";
+            EnrollmentDate = DateTime.Now;
+        }
+
+        public Student(int id, string first, string last, DateTime date)
+        {
+            StudentID = id;
+            FirstName = first;
+            LastName = last;
+            EnrollmentDate = date;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {StudentID}, Name: {FirstName} {LastName}, Enrollment: {EnrollmentDate.ToShortDateString()}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student Management System");
+
+            Student student1 = new Student();
+            student1.DisplayInfo();
+
+            Student student2 = new Student(101, "Binamra", "Kafle", new DateTime(2025, 7, 23));
+            student2.DisplayInfo();
+        }
+    }
+}
